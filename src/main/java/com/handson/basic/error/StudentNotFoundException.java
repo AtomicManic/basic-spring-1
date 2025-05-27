dc49c31aa8d9206e265b25fd1d1c1928fd51584e
adva <adva.apelboim@gmail.com>
2025-05-27 23:24:45 +0300

    with auth and error handler

---
+++ src/main/java/com/handson/basic/error/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.handson.basic.error;
+
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(Long id) {
+        super("User not found: " + id);
+    }
+}
+
